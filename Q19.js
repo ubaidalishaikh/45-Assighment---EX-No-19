56e5e724bddbec5f56f464ecececca247386d40e
ubaidalishaikh <161380272+ubaidalishaikh@users.noreply.github.com>
2024-04-01 03:09:57 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Q14_1 = require("../Exersice No14/Q14");
+console.log("Printing number of guest invited");
+console.log("We had finally invited ".concat(Q14_1.dinnerInvite.length, " people from Excersice 14: "));
---
+++ Q19.ts
@@ -0,0 +1,5 @@
+import{ dinnerInvite } from "../Exersice No14/Q14"
+
+console.log(`Printing number of guest invited`);
+console.log(`We had finally invited ${dinnerInvite.length} people from Excersice 14: `);
+
